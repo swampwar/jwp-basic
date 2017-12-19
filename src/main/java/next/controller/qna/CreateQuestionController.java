dd5ce0aa400224e4d52b432ed2a0b6df26f3d643
swampwar <dkdfl88@gmail.com>
2017-12-19 10:36:25 +0900

    반영

--- src/main/java/next/controller/qna/CreateQuestionController.java
+++ src/main/java/next/controller/qna/CreateQuestionController.java
@@ -1,7 +1,10 @@
 package next.controller.qna;
 
+import java.nio.file.AccessDeniedException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +13,7 @@ import core.mvc.AbstractController;
 import core.mvc.ModelAndView;
 import next.dao.QuestionDao;
 import next.model.Question;
+import next.model.User;
 
 public class CreateQuestionController extends AbstractController {
     private static final Logger log = LoggerFactory.getLogger(CreateQuestionController.class);
@@ -18,12 +22,21 @@ public class CreateQuestionController extends AbstractController {
 
     @Override
     public ModelAndView execute(HttpServletRequest req, HttpServletResponse response) throws Exception {
-        Question question = new Question(req.getParameter("writer"),req.getParameter("title"),
-        		req.getParameter("contents"));
+        // 로그인 체크
+    	// TODO 세션을 만든적이 없는데 반환되는 세션객체는 뭐지..??
+    	HttpSession session = req.getSession(false);
+    	if(session == null || session.getAttribute("user") == null){
+    		log.debug("세션에 user 속성없음");
+    		throw new AccessDeniedException("세션에 user 속성 없음");
+    	}
+    	
+    	// 세션의 name을 글쓴이로 한다.
+    	User loginUser = (User) session.getAttribute("user");
+    	Question question = new Question(loginUser.getName(),req.getParameter("title"),req.getParameter("contents"));
         log.debug("question : {}",question);
         
         questionDao.insert(question);
-        
-        return jspView("redirect:/");
+
+        return jsonView().addObject("insertRslt", true);
     }
 }
--- webapp/qna/form.jsp
+++ webapp/qna/form.jsp
@@ -9,7 +9,6 @@
 </head>
 <body>
 <%@ include file="/include/navigation.jspf" %>
-
 <div class="container" id="main">
    <div class="col-md-12 col-sm-12 col-lg-10 col-lg-offset-1">
       <div class="panel panel-default content-main">
@@ -34,5 +33,39 @@
 </div>
 
 <%@ include file="/include/footer.jspf" %>
+<script type="text/javascript">
+$('form[name=question] button[type=submit]').click(createQuestion);
+
+function createQuestion(e){
+	e.preventDefault();
+	
+	var queryString = $("form[name=question]").serialize();
+	 
+	$.ajax({
+	  type : 'post',
+	  url : '/qna/create',
+	  data : queryString,
+	  dataType : 'json',
+	  error: onError,
+	  success : onSuccess,
+	});
+}
+
+function onSuccess(json, status){
+	if(true == json.insertRslt){
+		// 등록 성공시 페이지 이동
+		$('form[name=question]').attr('action','/');
+		$('form[name=question]').submit();
+	}
+}
+
+//jqXHR jqXHR, String textStatus, String errorThrown
+function onError(xhr, status, errorThrown) {
+	// TODO 세션에 user 속성이 없으면 AccessDeniedException 발생시켰으나 ServletException 으로 던짐
+	// 미로그인으로 인한 예외인지 알지 못하고 처리하고 있음.
+	alert('로그인이 필요합니다.');
+}
+
+</script>
 </body>
 </html>
