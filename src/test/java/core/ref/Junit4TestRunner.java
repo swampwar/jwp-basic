94132eb421fc49d0478347b09b6b9c7025a79de4
swampwar <dkdfl88@gmail.com>
2018-01-23 15:55:09 +0900

    10장 리플렉션 개발중

--- src/test/java/core/ref/Junit4TestRunner.java
+++ src/test/java/core/ref/Junit4TestRunner.java
@@ -1,11 +1,24 @@
 package core.ref;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 import org.junit.Test;
+
+import next.model.User;
 
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
+    	// Junit4Test.java의 @MyTest 어노테이션이 붙은 메서드를 실행한다.
         Class<Junit4Test> clazz = Junit4Test.class;
-
+        Method[] methods = clazz.getMethods();
+        
+        for(Method method : methods){
+        	if(method.isAnnotationPresent(MyTest.class)){
+        		method.invoke(clazz.newInstance(), null);
+        	}
+        }
     }
 }
--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -2,6 +2,7 @@ package core.ref;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import org.junit.Test;
@@ -14,7 +15,6 @@ import next.model.User;
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
-    @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
 
@@ -40,14 +40,38 @@ public class ReflectionTest {
     }
     
     @Test
-    public void newInstanceWithConstructorArgs() {
+    public void newInstanceWithConstructorArgs() throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
         Class<User> clazz = User.class;
+        User user = null;
         logger.debug(clazz.getName());
+        
+    	// User 클래스의 객체를 생성해본다.(기본 생성자 없음)
+    	Constructor[] cons = clazz.getDeclaredConstructors();
+    	for(Constructor constructor : cons){
+    		// 기본생성자가 없기때문에 newInstance()로 생성할 수 없다.
+    		user = (User)constructor.newInstance(new Object[]{new String("userId"),new String("pw"),new String("name"),new String("email")});
+    	}
+    	
+    	logger.debug(user.toString());
     }
     
     @Test
-    public void privateFieldAccess() {
+    public void privateFieldAccess() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
         Class<Student> clazz = Student.class;
         logger.debug(clazz.getName());
+        
+        // 리플렉션 API를 이용하여 private 필드에 접근한다.
+        Student student = new Student();
+        Field nameField = clazz.getDeclaredField("name");
+        Field ageField = clazz.getDeclaredField("age");
+        
+        nameField.setAccessible(true);
+        nameField.set(student, "name_");
+        
+        ageField.setAccessible(true);
+        ageField.set(student, 31);
+        
+        logger.debug("name : {}, age : {}", student.getName(), student.getAge());
+        
     }
 }
